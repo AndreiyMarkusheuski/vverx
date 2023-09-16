118b5a4156470efdb6c7e652108cad212c3e5f73
fs.as.8585@gmail.com <41293675+AndreiyMarkusheuski@users.noreply.github.com>
2023-09-16 12:47:38 +0300

    Init pages, add fonts, add favicon and title, config styles'

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'catalog/index.html')
+      }
+    }
+  }
 });
