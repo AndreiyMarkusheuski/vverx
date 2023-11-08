53c3d8530711769d787887ab2274c1d7dedab86e
fs.as.8585@gmail.com <41293675+AndreiyMarkusheuski@users.noreply.github.com>
2023-11-08 12:27:51 +0300

    Minor fixes

--- src/scripts/smooth-scroll.js
+++ src/scripts/smooth-scroll.js
@@ -4,7 +4,7 @@ new fullpage("#smooth_scroll", {
   licenseKey: "C7F41B00-5E824594-9A5EFB99-B556A3D5",
   anchors: ["describe", "products", "about_us", "contacts_footer"],
   menu: "#menu",
-  scrollingSpeed: 900,
+  scrollingSpeed: 1200,
   scrollOverflow: true,
   easing: "easeInOutCubic",
   responsiveWidth: 876,
