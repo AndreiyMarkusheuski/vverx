a2357d033bf5e48717241e37350b3fe28ec2c3ca
fs.as.8585@gmail.com <41293675+AndreiyMarkusheuski@users.noreply.github.com>
2024-02-21 18:39:23 +0300

    Updates

---
+++ assets/swiper-product-34538919.js
@@ -0,0 +1,1 @@
+import{b as e,P as i}from"./vendor-e379e527.js";e.use([i]);new e(".detailed_card-swiper",{slidesPerView:"1",spaceBetween:20,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
