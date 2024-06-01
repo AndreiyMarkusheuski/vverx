404ecf105a416b509632b6ef0022bdd1a143c817
fs.as.8585@gmail.com <41293675+AndreiyMarkusheuski@users.noreply.github.com>
2024-06-01 20:25:35 +0300

    Updates

---
+++ assets/swiper-product-8057f586.js
@@ -0,0 +1,1 @@
+import{b as e,P as i}from"./vendor-53b96c22.js";e.use([i]);new e(".detailed_card-swiper",{slidesPerView:"1",autoHeight:!0,spaceBetween:20,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
--- assets/swiper-product-c2f2147d.js
+++
@@ -1,1 +0,0 @@
-import{b as e,P as i}from"./vendor-e379e527.js";e.use([i]);new e(".detailed_card-swiper",{slidesPerView:"1",autoHeight:!0,spaceBetween:20,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
